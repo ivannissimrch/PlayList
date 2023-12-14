eca581fc180c29a46e982736cecbb96ce79f37df
ivannissimrch <ivannissimrch@gmail.com>
2023-12-13 21:59:48 -0500

    Add search input on Search Page

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,3 +1,31 @@
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+
 export default function LibraryPage() {
-  return <div>PlayListPage</div>;
+  const DemoPaper = styled(Paper)(({ theme }) => ({
+    width: 120,
+    height: 120,
+    padding: theme.spacing(2),
+    ...theme.typography.body2,
+    textAlign: "center",
+  }));
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "98%",
+      }}
+    >
+      <DemoPaper square>PlayList 1</DemoPaper>
+      <DemoPaper square>PlayList 2</DemoPaper>
+      <DemoPaper square>PlayList 1</DemoPaper>
+      <DemoPaper square>PlayList 2</DemoPaper>
+    </Stack>
+  );
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,28 @@
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
 export default function SearchPage() {
-  return <div>SearchPage</div>;
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Song"
+        inputProps={{ "aria-label": "search Song" }}
+      />
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 }
