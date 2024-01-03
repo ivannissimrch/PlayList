53e67ffa5e63a9c7e21c2de97203feff6cc5d220
ivannissimrch <ivannissimrch@gmail.com>
2024-01-02 22:18:02 -0500

    Add a delete button to the songs in the playlist

--- src/pages/PlayListPage.jsx
+++ src/pages/PlayListPage.jsx
@@ -6,27 +6,32 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import playSelectedSong from "../helpers/playSelectedSong";
+import { Button } from "@mui/material";
+import deleteSongFromPayList from "../helpers/deleteSongFromPlayList";
 
 export default function PlayListPage({ token }) {
   const songs = useLoaderData();
   const navigate = useNavigate();
+  const { playlistId } = useParams();
 
   function handleOnClick(value) {
     playSelectedSong("play", "PUT", token, value.track.uri);
     navigate("/");
   }
 
+  function handleDelete(value) {
+    deleteSongFromPayList(playlistId, value.track.uri, "DELETE", token);
+    navigate("/");
+  }
+
   return (
     <List dense sx={{ width: "100%", bgcolor: "background.paper" }}>
       {songs.map((value) => {
-        {
-          /* const labelId = `checkbox-list-secondary-label-${value}`; */
-        }
         return (
-          <ListItem key={value.track.id} onClick={() => handleOnClick(value)}>
-            <ListItemButton>
+          <ListItem key={value.track.id}>
+            <ListItemButton onClick={() => handleOnClick(value)}>
               <ListItemAvatar>
                 <Avatar
                   alt="Album image"
@@ -38,6 +43,7 @@ export default function PlayListPage({ token }) {
                 primary={`${value.track.name} ${value.track.album.artists[0].name} `}
               />
             </ListItemButton>
+            <Button onClick={() => handleDelete(value)}>Delete</Button>
           </ListItem>
         );
       })}
