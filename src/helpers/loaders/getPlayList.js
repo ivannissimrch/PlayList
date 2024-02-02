4c7a59de3d7e2beb56ef75d5cdb52e9ffceebf71
ivannissimrch <ivannissimrch@gmail.com>
2024-02-01 20:57:45 -0500

    Remove unnecessary try-catch from loaders

--- src/helpers/loaders/getPlayList.js
+++ src/helpers/loaders/getPlayList.js
@@ -1,9 +1,4 @@
 export default async function getPlayList(spotifyApi) {
-  try {
-    const response = await spotifyApi.getUserPlaylists();
-    return response.items;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  const response = await spotifyApi.getUserPlaylists();
+  return response.items;
 }
--- src/helpers/loaders/getPlayListSongs.js
+++ src/helpers/loaders/getPlayListSongs.js
@@ -1,9 +1,4 @@
 export default async function getPlayListSongs({ playlistId }, spotifyApi) {
-  try {
-    const response = await spotifyApi.getPlaylistTracks(playlistId);
-    return response.items;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  const response = await spotifyApi.getPlaylistTracks(playlistId);
+  return response.items;
 }
